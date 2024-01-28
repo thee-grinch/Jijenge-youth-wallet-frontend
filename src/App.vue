e211397b2b29410f9336de4719f6a769981686da
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-28 13:07:09 +0300

    trying

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <NavBar />
+  <HeroSection />
+  <ContributorsList /> -->
+    <!-- <FirstPage /> -->
+    <!-- <LogIn />
+    <SignUp /> -->
+    <LoginPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import FirstPage from "./components/FirstPage.vue";
+// import NavBar from './components/NavBar.vue';
+// import HeroSection from './components/HeroSection.vue';
+// import ContributorsList from './components/Contributions.vue'
+// import LogIn from './components/LogIn.vue';
+// import SignUp from './components/SignUp.vue';
+import LoginPage from './components/LoginPage.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // // HelloWorld,
+    // NavBar,
+    // HeroSection,
+    // ContributorsList
+    // FirstPage
+    // LogIn,
+    // SignUp
+    LoginPage
+}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
